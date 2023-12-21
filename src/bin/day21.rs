2580df5850e68c26001591f70e96757861eb5507
Rowan Hill <rowan.hill@gmail.com>
2023-12-21 09:27:05 +0000

    Day 21 brute force

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -2,9 +2,9 @@ use std::collections::HashSet;
 
 enum Direction { North, East, South, West }
 
-struct Bounds { width: usize, height: usize }
+struct Bounds { width: isize, height: isize }
 #[derive(PartialEq, Eq, Hash, Clone)]
-struct Coord { x: usize, y: usize }
+struct Coord { x: isize, y: isize }
 impl Coord {
     fn bounded_step(&self, dir: &Direction, bounds: &Bounds) -> Option<Coord> {
         match dir {
@@ -12,6 +12,15 @@ impl Coord {
             Direction::East => if self.x < bounds.width - 1 { Some(Coord { x: self.x + 1, y: self.y }) } else { None },
             Direction::South => if self.y < bounds.height - 1 { Some(Coord { x: self.x, y: self.y + 1 }) } else { None },
             Direction::West => if self.x > 0 { Some(Coord { x: self.x - 1, y: self.y }) } else { None },
+        }
+    }
+
+    fn step(&self, dir: &Direction) -> Coord {
+        match dir {
+            Direction::North => Coord { x: self.x, y: self.y - 1 },
+            Direction::East => Coord { x: self.x + 1, y: self.y },
+            Direction::South => Coord { x: self.x, y: self.y + 1 },
+            Direction::West => Coord { x: self.x - 1, y: self.y },
         }
     }
 }
@@ -29,7 +38,7 @@ impl Map {
             let mut row = Vec::new();
             for (x, char) in line.chars().enumerate() {
                 if char == 'S' {
-                    start = Some(Coord { x, y });
+                    start = Some(Coord { x: x as isize, y: y as isize });
                     row.push('.');
                 } else {
                     row.push(char);
@@ -38,31 +47,42 @@ impl Map {
             width = row.len();
             grid.push(row);
         }
-        (Map { bounds: Bounds { width, height: grid.len() }, grid }, start.unwrap())
+        (Map { bounds: Bounds { width: width as isize, height: grid.len() as isize }, grid }, start.unwrap())
     }
 
     fn get(&self, coord: &Coord) -> Option<char> {
         if coord.x < self.bounds.width && coord.y < self.bounds.height {
-            Some(self.grid[coord.y][coord.x])
+            Some(self.grid[coord.y as usize][coord.x as usize])
         } else {
             None
         }
     }
 
-    fn is_open(&self, coord: &Coord) -> bool {
-        self.get(coord).map(|c| c == '.').unwrap_or(false)
+    fn get_wrapped(&self, coord: &Coord) -> char {
+        let y = coord.y.rem_euclid(self.bounds.height) as usize;
+        let x = coord.x.rem_euclid(self.bounds.width) as usize;
+        self.grid[y][x]
     }
 
-    fn try_move(&self, start: &Coord, dir: &Direction) -> Option<Coord> {
-        start.bounded_step(dir, &self.bounds).filter(|c| self.is_open(c))
+    fn is_open(&self, coord: &Coord, allow_wrapping: bool) -> bool {
+        let char = if allow_wrapping { Some(self.get_wrapped(coord)) } else { self.get(coord) };
+        char.map(|c| c == '.').unwrap_or(false)
+    }
+
+    fn try_move(&self, start: &Coord, dir: &Direction, allow_wrapping: bool) -> Option<Coord> {
+        if allow_wrapping {
+            Some(start.step(dir)).filter(|c| self.is_open(c, allow_wrapping))
+        } else {
+            start.bounded_step(dir, &self.bounds).filter(|c| self.is_open(c, allow_wrapping))
+        }
     }
 }
 
-fn find_next_steps(starts: HashSet<Coord>, map: &Map) -> HashSet<Coord> {
+fn find_next_steps(starts: HashSet<Coord>, map: &Map, allow_wrapping: bool) -> HashSet<Coord> {
     let mut next_steps = HashSet::new();
     for start in starts {
         for dir in [Direction::North, Direction::East, Direction::South, Direction::West] {
-            if let Some(next_step) = map.try_move(&start, &dir) {
+            if let Some(next_step) = map.try_move(&start, &dir, allow_wrapping) {
                 next_steps.insert(next_step);
             }
         }
@@ -70,23 +90,27 @@ fn find_next_steps(starts: HashSet<Coord>, map: &Map) -> HashSet<Coord> {
     next_steps
 }
 
-fn find_positions_after(start: &Coord, steps: usize, map: &Map) -> HashSet<Coord> {
+fn find_positions_after(start: &Coord, steps: usize, map: &Map, allow_wrapping: bool) -> HashSet<Coord> {
     let mut positions = HashSet::new();
     positions.insert(start.clone());
-    for _ in 0..steps {
-        positions = find_next_steps(positions, map);
+    for i in 0..steps {
+        if i % 500 == 0 {
+            println!("{} steps completed ({} %)", i, i * 100 / steps);
+        }
+        positions = find_next_steps(positions, map, allow_wrapping);
     }
     positions
 }
 
-fn find_num_positions_after(start: &Coord, steps: usize, map: &Map) -> usize {
-    find_positions_after(start, steps, map).len()
+fn find_num_positions_after(start: &Coord, steps: usize, map: &Map, allow_wrapping: bool) -> usize {
+    find_positions_after(start, steps, map, allow_wrapping).len()
 }
 
 fn main() {
     let input = include_str!("../../input/day21");
     let (map, start) = Map::parse(input);
-    println!("Part 1: {}", find_num_positions_after(&start, 64, &map));
+    println!("Part 1: {}", find_num_positions_after(&start, 64, &map, false));
+    println!("Part 2: {}", find_num_positions_after(&start, 26501365, &map, false));
 }
 
 #[cfg(test)]
@@ -107,9 +131,32 @@ mod tests {
 .##..##.##.
 ...........";
         let (map, start) = Map::parse(input);
-        assert_eq!(find_num_positions_after(&start, 1, &map), 2);
-        assert_eq!(find_num_positions_after(&start, 2, &map), 4);
-        assert_eq!(find_num_positions_after(&start, 3, &map), 6);
-        assert_eq!(find_num_positions_after(&start, 6, &map), 16);
+        assert_eq!(find_num_positions_after(&start, 1, &map, false), 2);
+        assert_eq!(find_num_positions_after(&start, 2, &map, false), 4);
+        assert_eq!(find_num_positions_after(&start, 3, &map, false), 6);
+        assert_eq!(find_num_positions_after(&start, 6, &map, false), 16);
+    }
+
+    #[test]
+    fn test_find_num_positions_after_wrapping() {
+        let input = "...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........";
+        let (map, start) = Map::parse(input);
+        // assert_eq!(find_num_positions_after(&start, 6, &map, true), 16);
+        // assert_eq!(find_num_positions_after(&start, 10, &map, true), 50);
+        // assert_eq!(find_num_positions_after(&start, 50, &map, true), 1594);
+        // assert_eq!(find_num_positions_after(&start, 100, &map, true), 6536);
+        assert_eq!(find_num_positions_after(&start, 500, &map, true), 167004);
+        // assert_eq!(find_num_positions_after(&start, 1000, &map, true), 668697);
+        // assert_eq!(find_num_positions_after(&start, 5000, &map, true), 16733044);
     }
 }
